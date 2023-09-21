8809fbe418d31546de1a591ffd8ff02cd6c19cd8
sajid <sajidxullah@gmail.com>
2023-09-21 14:29:06 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: "/github-pages",
+  // basePath: "/github-pages",
 };
 
 module.exports = nextConfig;
